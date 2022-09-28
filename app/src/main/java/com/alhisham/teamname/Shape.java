6062b10a56aba2f23edbaca4479644b539fa855c
Hishamanik <anikalhisham@gmail.com>
2022-09-27 18:12:54 -0600

    new change

--- app/src/main/java/com/alhisham/teamname/Shape.java
+++ app/src/main/java/com/alhisham/teamname/Shape.java
@@ -1,2 +1,11 @@
-package com.alhisham.teamname;public class Shape {
+package com.alhisham.teamname;
+
+public class Shape {
+    public int x;
+    public int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
